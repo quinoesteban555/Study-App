e243d1a4e60dfd869630f8bd71e466b937750a18
quinoesteban555 <69019687+quinoesteban555@users.noreply.github.com>
2023-09-05 01:37:00 -0500

    Esteban (#2)
    * no message
    
    * no message

--- client/src/App.js
+++ client/src/App.js
@@ -2,71 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from "react";
 import axios from "axios";
+import About from './pages/about';
+import Answer from './pages/answer';
+import Navbar from './components/index.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
-  const [question, setquestion] = useState("");
-  const [answer, setanswer] = useState("");
-  const GetQuestion = () => {
-    axios.get("http://localhost:5000/api")
-    .then((response)=>
-    {
-      if (response.status === 200)
-      {
-        console.log("I hear ye");
-        let array = response.data.question;
-        let num = Math.floor(Math.random() * array.length);
-        setquestion(array[num]);
-      }
-      else
-      {
-        console.log(response.data.message);
-        alert(response.data.message);
-      }
-    })
-    .catch((error) => 
-    {
-        console.log(error);
-    })};
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      const send = [answer]
-      axios
-          .post("http://localhost:5000/api/response", send)
-          .then((response) => {console.log("info sent"); 
-        if (response.status === 200)
-        {
-          GetQuestion();
-        }
-        })
-          .catch((err) => {
-              console.error(err);
-            });
-
-        };
-  const handleUserInput = async (e) => {
-      const name = e.target.name
-      const value = e.target.value;
-      setanswer(value);
-  }
   return (
-    <>
-    <div className="App">
-      {useEffect(() => {
-      GetQuestion()
-    },[])}
-      <header className="App-header">
-      Study Time
-      <form className='Input'> 
-      <label htmlFor="location"> {question} </label>
-        <br />
-        <input type="text" name="answer" onChange={handleUserInput}/>
-        <br />
-        <input type="submit" value="Submit" onClick={handleSubmit} />
-        <br />
-      </form>
-      </header>
-    </div>
-    </>
+    <Router>
+      <Navbar />
+    <Routes>
+    <Route path='/about' exact component={About} />
+    <Route path='/answer' component={Answer()} />
+    </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/components/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
+} from './NavbarElements';
+  
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <Bars />
+  
+        <NavMenu>
+          <NavLink to='/about' activeStyle>
+            About
+          </NavLink>
+          <NavLink to='/answer' activeStyle>
+            Answer
+          </NavLink>
+        </NavMenu>
+      </Nav>
+    </>
+  );
+};
+  
+export default Navbar;
