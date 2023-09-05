b221ffc4d5c570d7de4bffdb5144e838afadd7a0
quinoesteban555 <quinoesteban555@gmail.com>
2023-09-05 01:30:42 -0500

    no message

---
+++ client/src/pages/answer.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const Answer = () => {
+    const [question, setquestion] = useState("");
+  const [answer, setanswer] = useState("");
+  const GetQuestion = () => {
+    axios.get("http://localhost:5000/api")
+    .then((response)=>
+    {
+      if (response.status === 200)
+      {
+        console.log("I hear ye");
+        let array = response.data.question;
+        let num = Math.floor(Math.random() * array.length);
+        setquestion(array[num]);
+      }
+      else
+      {
+        console.log(response.data.message);
+        alert(response.data.message);
+      }
+    })
+    .catch((error) => 
+    {
+        console.log(error);
+    })};
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      const send = [answer]
+      axios
+          .post("http://localhost:5000/api/response", send)
+          .then((response) => {console.log("info sent"); 
+        if (response.status === 200)
+        {
+          GetQuestion();
+        }
+        })
+          .catch((err) => {
+              console.error(err);
+            });
+
+        };
+  const handleUserInput = async (e) => {
+      const name = e.target.name
+      const value = e.target.value;
+      setanswer(value);
+  }
+  return (
+    <>
+    <div className="App">
+      {useEffect(() => {
+      GetQuestion()
+    },[])}
+      <header className="App-header">
+      Study Time
+      <form className='Input'> 
+      <label htmlFor="location"> {question} </label>
+        <br />
+        <input type="text" name="answer" onChange={handleUserInput}/>
+        <br />
+        <input type="submit" value="Submit" onClick={handleSubmit} />
+        <br />
+      </form>
+      </header>
+    </div>
+    </>
+  )
+}
+export default Answer
